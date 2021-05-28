7b481c2184facd6a66588f66f7d15324c5d52d7b
Scott <76458930+IDMiller2020@users.noreply.github.com>
2021-05-28 12:40:16 -0600

    initial client and serve setup progress

--- keeper.client/src/env.js
+++ keeper.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const baseURL = dev ? 'https://localhost:5001' : ''
+export const domain = 'dev-2rkcetm8.us.auth0.com'
+export const audience = 'keeper.com'
+export const clientId = 'NEoP3Ww7f0feLlO1cGqoSopAo5ID1YcV'
