4c5e96284a917d9514dca6db0848d6cc4f91f4bb
Scott <76458930+IDMiller2020@users.noreply.github.com>
2021-05-29 11:40:25 -0600

    Server models completed

---
+++ keeper.server/Models/VaultKeep.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace keeper.server.Models
+{
+  public class VaultKeep
+  {
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public int VaultId { get; set; }
+    public int KeepId { get; set; }
+    public string CreatorId { get; set; }
+  }
+}
